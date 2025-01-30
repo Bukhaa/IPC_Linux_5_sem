5a352c561b60c72fe05b4e3f660aa67f437274cb
Alexander <165910751+Bukhaa@users.noreply.github.com>
2025-01-30 17:52:26 +0700

    Update main.c

--- traceroute/main.c
+++ traceroute/main.c
@@ -9,16 +9,14 @@
 #include <sys/socket.h>
 #include <linux/rtnetlink.h>
 #include <time.h>
-#include <netdb.h> // gethostbyaddr
+#include <netdb.h>
 
 #define ARP_FILE "/proc/net/arp"
 #define LOG_FILE "network_log.txt"
-#define DOT_FILE "network_graph.dot"
-#define PNG_FILE "network_graph.png"
 #define SCAN_INTERVAL 60
 #define MAX_LINE_LENGTH 256
 
-// Функция для получения MAC-адреса из строки
+// Функция для определения MAC-адреса из строки (как и раньше)
 void get_mac_from_line(char *line, char *mac) {
     char *ptr = strtok(line, " ");
     int i = 0;
@@ -33,97 +31,17 @@ void get_mac_from_line(char *line, char *mac) {
     strcpy(mac, "N/A");
 }
 
-// Функция для определения шлюза (очень базовый метод)
-int get_default_gateway(char *gateway_ip)
-{
-    FILE *fp;
-    char line[MAX_LINE_LENGTH];
-    char interface[IF_NAMESIZE];
-    unsigned long dest_addr, gw_addr;
-    int if_idx;
-
-    fp = fopen("/proc/net/route", "r");
-    if(fp == NULL) {
-        return -1;
-    }
-
-    // Пропускаем заголовок
-    fgets(line, MAX_LINE_LENGTH, fp);
-    while(fgets(line, MAX_LINE_LENGTH, fp)) {
-        if (sscanf(line, "%s %lx %lx %*x %*x %*x %*x %*x %*x %d", interface, &dest_addr, &gw_addr, &if_idx) == 4) {
-            if(dest_addr == 0) {
-               inet_ntop(AF_INET, &gw_addr, gateway_ip, INET_ADDRSTRLEN);
-               fclose(fp);
-               return 0;
-             }
-
-        }
-    }
-
-    fclose(fp);
-    strcpy(gateway_ip, "N/A");
-    return -1;
-}
-
-void render_dot_to_png(const char *dot_file, const char *png_file) {
-    char command[256];
-    snprintf(command, sizeof(command), "dot -Tpng -o %s %s", png_file, dot_file);
-
-    int result = system(command);
-    if (result != 0) {
-        fprintf(stderr, "Ошибка при рендеринге .dot в PNG\n");
-    } else {
-        printf("Файл %s успешно создан.\n", png_file);
-    }
-}
-
-void generate_dot_file(const char *gateway_ip, char nodes[256][INET_ADDRSTRLEN], int node_count) {
-    FILE *fp = fopen(DOT_FILE, "w");
-    if (fp == NULL) {
-        perror("Error opening dot file");
-        return;
-    }
-
-    fprintf(fp, "digraph Network {\n");
-    fprintf(fp, "    rankdir=LR;\n");
-      fprintf(fp, "    node [shape=box];\n");
-    fprintf(fp, "    \"%s\" [shape=house, color=blue];\n", gateway_ip);
-
-
-    for (int i = 0; i < node_count; i++) {
-      if (strcmp(nodes[i], gateway_ip) != 0){
-            fprintf(fp, "    \"%s\" -> \"%s\";\n",gateway_ip, nodes[i]);
-      }
-    }
-
-    fprintf(fp, "}\n");
-
-    fclose(fp);
-}
-
-
 void scan_network() {
     FILE *fp;
     char line[MAX_LINE_LENGTH];
     char ip_addr[INET_ADDRSTRLEN];
     char mac_addr[18];
     char hostname[NI_MAXHOST];
-    char current_gateway[INET_ADDRSTRLEN];
     time_t now;
     time(&now);
     char time_str[26];
-    int node_count = 0;
-    char nodes[256][INET_ADDRSTRLEN];
-
-
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", localtime(&now));
 
-    if(get_default_gateway(current_gateway) == 0) {
-        printf("Default gateway: %s\n", current_gateway);
-    }
-    else {
-        printf("Could not find default gateway\n");
-    }
 
     fp = fopen(ARP_FILE, "r");
     if (fp == NULL) {
@@ -137,16 +55,13 @@ void scan_network() {
         fclose(fp);
         return;
     }
-
-
     fprintf(log_fp, "==== %s ====\n", time_str);
-    fprintf(log_fp, "Default gateway: %s\n", current_gateway);
+
 
     fgets(line, MAX_LINE_LENGTH, fp); // skip header
     while (fgets(line, MAX_LINE_LENGTH, fp) != NULL) {
         if (sscanf(line, "%s %*s %*s %*s %s %*s\n", ip_addr, mac_addr) == 2) {
-
-            // Resolve hostname
+             // Resolve hostname
             struct sockaddr_in addr;
             inet_pton(AF_INET, ip_addr, &(addr.sin_addr));
              socklen_t addrlen = sizeof(addr);
@@ -158,40 +73,30 @@ void scan_network() {
 
             fprintf(log_fp, "IP: %s, MAC: %s, Hostname: %s\n", ip_addr, mac_addr, hostname);
             printf("IP: %s, MAC: %s, Hostname: %s\n", ip_addr, mac_addr, hostname);
-
-            strcpy(nodes[node_count], ip_addr);
-            node_count++;
         }
-
-        }
+    }
     fclose(log_fp);
     fclose(fp);
-
-
-    generate_dot_file(current_gateway, nodes, node_count);
-    render_dot_to_png(DOT_FILE, PNG_FILE);
 }
-
 
 int main() {
     pid_t pid, sid;
-    pid = fork();
 
-    if(pid < 0) {
+    pid = fork();
+    if (pid < 0) {
         exit(EXIT_FAILURE);
     }
-
-    if(pid > 0) {
+    if (pid > 0) {
         exit(EXIT_SUCCESS);
     }
 
     umask(0);
 
     sid = setsid();
-
-    if(sid < 0) {
+    if (sid < 0) {
         exit(EXIT_FAILURE);
     }
+
 
     while (1) {
         scan_network();
